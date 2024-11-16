222d83a5f65a688bf995eafcb0465069a42e1c0d
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-11-15 22:38:51 -0500

    scrolls

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:recetasappg10/pages/home_page.dart';
 import 'package:recetasappg10/widgets_pages/list_view_page.dart';
+import 'package:recetasappg10/widgets_pages/scroll_page.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home: ListViewPage(),
+      home: ScrollPage(),
       debugShowCheckedModeBanner: false,
     ),
   );
---
+++ lib/widgets_pages/scroll_page.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class ScrollPage extends StatelessWidget {
+  Widget buildContainers(String text) {
+    return Container(
+      height: 50,
+      width: 200,
+      color: Colors.red,
+      margin: EdgeInsets.all(16),
+      child: Center(
+        child: Text(text),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Scroll Page"),
+      ),
+      body: SingleChildScrollView(
+        // scrollDirection: Axis.horizontal,
+        child: Column(
+          children: [
+            Text("CABECERA 1"),
+            SizedBox(
+              height: 200,
+              child: ListView.builder(
+                itemCount: 10,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) => buildContainers("111"),
+              ),
+            ),
+            buildContainers("2"),
+            buildContainers("2"),
+            ...List.generate(
+              5,
+              (index) => Padding(
+                padding: EdgeInsets.all(16),
+                child: Text("HIJO"),
+              ),
+            ),
+            buildContainers("3"),
+            buildContainers("3"),
+            SizedBox(
+              height: 200,
+              child: ListView.separated(
+                  itemBuilder: (context, index) =>
+                      buildContainers("separated $index"),
+                  separatorBuilder: (context, index) => Container(
+                        width: 20,
+                        height: 20,
+                        color: Colors.black,
+                      ),
+                  itemCount: 20),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
