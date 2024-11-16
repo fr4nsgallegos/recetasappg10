f137a80c05f5628c7efc14dc95797fe2e9c456e2
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-11-15 22:12:10 -0500

    listview page

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:recetasappg10/pages/home_page.dart';
+import 'package:recetasappg10/widgets_pages/list_view_page.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home: HomePage(),
+      home: ListViewPage(),
       debugShowCheckedModeBanner: false,
     ),
   );
---
+++ lib/widgets_pages/list_view_page.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class ListViewPage extends StatefulWidget {
+  @override
+  State<ListViewPage> createState() => _ListViewPageState();
+}
+
+class _ListViewPageState extends State<ListViewPage> {
+  List<String> letrasList = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "a",
+    "s",
+    "d"
+  ];
+
+  Widget buildContainers(String text) {
+    return Container(
+      height: 50,
+      width: 200,
+      color: Colors.red,
+      margin: EdgeInsets.all(16),
+      child: Center(
+        child: Text(text),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        letrasList.add("x");
+        setState(() {});
+      }),
+      appBar: AppBar(
+        title: Text("List view Page"),
+      ),
+      body: ListView.builder(
+        itemCount: letrasList.length,
+        itemBuilder: (BuildContext context, int index) {
+          print(index);
+          return buildContainers(letrasList[index]);
+        },
+      ),
+    );
+  }
+}
