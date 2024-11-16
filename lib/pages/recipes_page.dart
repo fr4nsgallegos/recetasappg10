a7d34a66ce617b23b479da6b1b4218c1ca3992ff
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-11-16 12:45:44 -0500

    style form

--- lib/pages/recipes_page.dart
+++ lib/pages/recipes_page.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:recetasappg10/widgets/form_item_widget.dart';
 
 class RecipesPage extends StatelessWidget {
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _preparationController = TextEditingController();
+  final TextEditingController _urlController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      appBar: AppBar(title: Text('Desafio : Formulario de Receta')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             ////CREAR FORM DONDE PUEDA LLENAR TITULO, PREPARACIÓN, URL DE LA IMAGEN.
             ///CRITERIOS PARA EL VALIDATOR
@@ -13,9 +21,61 @@ class RecipesPage extends StatelessWidget {
             ///PREPARACIÓN: NO SEA NILL NI EMPTY Y AL MENOS 20 CARACTERES
             ///URL: NO SEA NULL NI EMPETY Y AL MENOS 10 CARACTERES
             ///
+            FormItemWidget(
+                controller: _titleController,
+                hint: "Ingresa el título",
+                icon: Icons.title),
+            FormItemWidget(
+              controller: _preparationController,
+              hint: "Ingresa la preparación",
+              icon: Icons.list,
+              maxLines: 3,
+            ),
+            FormItemWidget(
+                controller: _urlController,
+                hint: "Ingresa la url de la imagen",
+                icon: Icons.image),
+
+            SizedBox(height: 20),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  String title = _titleController.text.trim();
+                  String preparation = _preparationController.text.trim();
+                  String url = _urlController.text.trim();
+
+                  if (title.isEmpty || title.length < 5) {
+                    showError(
+                        context, 'El título debe tener al menos 5 caracteres.');
+                    return;
+                  }
+                  if (preparation.isEmpty || preparation.length < 20) {
+                    showError(context,
+                        'La preparación debe tener al menos 20 caracteres.');
+                    return;
+                  }
+                  if (url.isEmpty || url.length < 10) {
+                    showError(
+                        context, 'La URL debe tener al menos 10 caracteres.');
+                    return;
+                  }
+
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Formulario válido')),
+                  );
+                },
+                child: Text('Guardar'),
+              ),
+            ),
           ],
         ),
       ),
+    );
+  }
+
+  void showError(BuildContext context, String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(message, style: TextStyle(color: Colors.red))),
     );
   }
 }
---
+++ lib/widgets/form_item_widget.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class FormItemWidget extends StatelessWidget {
+  TextEditingController controller;
+  String hint;
+  IconData icon;
+  int maxLines;
+  FormItemWidget({
+    required this.controller,
+    required this.hint,
+    required this.icon,
+    this.maxLines = 1,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8),
+      child: TextField(
+        maxLines: maxLines,
+        style: TextStyle(color: Colors.white),
+        controller: controller,
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+          hintStyle: TextStyle(color: Colors.white),
+          prefixIcon: Icon(
+            icon,
+            color: Colors.white,
+          ),
+          hintText: hint,
+          filled: true,
+          fillColor: Color(0xff606c38),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.circular(25),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.circular(25),
+          ),
+        ),
+      ),
+    );
+  }
+}
