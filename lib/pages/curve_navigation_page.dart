c45b548e58fead443bb11c66b080a5d5b8326c36
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-11-17 12:47:05 -0500

    menu hamburguesa

--- lib/pages/curve_navigation_page.dart
+++ lib/pages/curve_navigation_page.dart
@@ -1,8 +1,6 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:recetasappg10/models/screen_model.dart';
-import 'package:recetasappg10/pages/home_page.dart';
-import 'package:recetasappg10/pages/recipes_page.dart';
 
 class CurveNavigationPage extends StatefulWidget {
   @override
@@ -14,46 +12,88 @@ class _CurveNavigationPageState extends State<CurveNavigationPage> {
   final pageController = PageController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          pageDetails[_activePageIndex].title,
-          style: TextStyle(color: Colors.white),
+    return SafeArea(
+      child: Scaffold(
+        drawer: Drawer(
+          //menu lateral a la izquierda
+          child: ListView(
+            children: [
+              DrawerHeader(
+                margin: EdgeInsets.all(0),
+                child: Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.all(32),
+                  child: FlutterLogo(
+                      // size: 20,
+                      ),
+                ),
+                decoration: BoxDecoration(color: Colors.black),
+              ),
+              //opciones del menu lateral
+              // ListTile(
+              //   leading: Icon(Icons.home),
+              //   title: Text("Home"),
+              //   onTap: () {},
+              // )
+
+              ...pageDetails
+                  .map(
+                    (e) => ListTile(
+                      leading: e.icono,
+                      title: Text(e.title),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => e.page),
+                        );
+                      },
+                    ),
+                  )
+                  .toList(),
+            ],
+          ),
         ),
-        backgroundColor: pageDetails[_activePageIndex].bgColor,
-        elevation: 10,
-      ),
-      bottomNavigationBar: CurvedNavigationBar(
-        items: [
-          ...pageDetails.map((e) => e.icono).toList(),
-        ],
-        index: _activePageIndex, //setea donde va a inciar el page
-        // height: 10,//altura del menu
-        color: Color(0xffccd5ae), //color de fondo de la barra
-        buttonBackgroundColor:
-            pageDetails[_activePageIndex].colorButtonOption, //Color del botón
-        backgroundColor: pageDetails[_activePageIndex]
-            .bgColor, //Color de fondo (debe ser similar a la pantalla que va a mostrar)
-        onTap: (index) {
-          pageController.animateToPage(
-            index,
-            duration: Duration(seconds: 1),
-            curve: Curves.decelerate,
-          );
-          // _activePageIndex = index;
-          setState(() {});
-        },
-      ),
-      body: PageView(
-        controller: pageController,
-        onPageChanged: (index) {
-          _activePageIndex = index;
-          setState(() {});
-        },
-        children: [
-          ...pageDetails.map((e) => e.page).toList(),
-        ],
-        // child: pageDetails[_activePageIndex].page
+        // endDrawer: Drawer(), //menu lateral a la derecha
+        appBar: AppBar(
+          title: Text(
+            pageDetails[_activePageIndex].title,
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: pageDetails[_activePageIndex].bgColor,
+          elevation: 10,
+        ),
+        bottomNavigationBar: CurvedNavigationBar(
+          items: [
+            ...pageDetails.map((e) => e.icono).toList(),
+          ],
+          index: _activePageIndex, //setea donde va a inciar el page
+          // height: 10,//altura del menu
+          color: Color(0xffccd5ae), //color de fondo de la barra
+          buttonBackgroundColor:
+              pageDetails[_activePageIndex].colorButtonOption, //Color del botón
+          backgroundColor: pageDetails[_activePageIndex]
+              .bgColor, //Color de fondo (debe ser similar a la pantalla que va a mostrar)
+          onTap: (index) {
+            pageController.animateToPage(
+              index,
+              duration: Duration(seconds: 1),
+              curve: Curves.decelerate,
+            );
+            // _activePageIndex = index;
+            setState(() {});
+          },
+        ),
+        body: PageView(
+          controller: pageController,
+          onPageChanged: (index) {
+            _activePageIndex = index;
+            setState(() {});
+          },
+          children: [
+            ...pageDetails.map((e) => e.page).toList(),
+          ],
+          // child: pageDetails[_activePageIndex].page
+        ),
       ),
     );
   }
