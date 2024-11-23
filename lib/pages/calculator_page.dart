a1493ee5ed13929df29ecbc3fe6275970c4cd102
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-11-22 19:59:40 -0500

    build buttons calculator

--- lib/pages/calculator_page.dart
+++ lib/pages/calculator_page.dart
@@ -31,14 +31,48 @@ class CalculatorPage extends StatelessWidget {
             SizedBox(
               height: 16,
             ),
-            Row(
-              children: [
-                BuildButton(),
-                BuildButton(),
-                BuildButton(),
-                BuildButton(),
-              ],
-            )
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Row(
+                      children: [
+                        BuildButton("7", Color(0xff202521), () {}),
+                        BuildButton("8", Color(0xff202521), () {}),
+                        BuildButton("9", Color(0xff202521), () {}),
+                        BuildButton("/", Color(0xff202521), () {}),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        BuildButton("4", Color(0xff202521), () {}),
+                        BuildButton("5", Color(0xff202521), () {}),
+                        BuildButton("6", Color(0xff202521), () {}),
+                        BuildButton("x", Color(0xff202521), () {}),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        BuildButton("1", Color(0xff202521), () {}),
+                        BuildButton("2", Color(0xff202521), () {}),
+                        BuildButton("3", Color(0xff202521), () {}),
+                        BuildButton("-", Color(0xff202521), () {}),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        BuildButton("0", Color(0xff202521), () {}),
+                        BuildButton(".", Color(0xff202521), () {}),
+                        BuildButton("<-", Color(0xff202521), () {}),
+                        BuildButton("=", Color(0xff202521), () {}),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
@@ -47,22 +81,33 @@ class CalculatorPage extends StatelessWidget {
 }
 
 class BuildButton extends StatelessWidget {
+  String texto;
+  Color bgColor;
+  VoidCallback funcion;
+
+  BuildButton(
+    this.texto,
+    this.bgColor,
+    this.funcion,
+  );
   @override
   Widget build(BuildContext context) {
-    return OutlinedButton(
-      style: OutlinedButton.styleFrom(
-        shape: CircleBorder(),
-        backgroundColor: Color(0xff202521),
-        side: BorderSide.none,
-      ),
-      onPressed: () {},
-      child: Container(
-        alignment: Alignment.center,
-        height: MediaQuery.of(context).size.height / 12,
-        width: MediaQuery.of(context).size.width / 8,
-        child: Text(
-          "1",
-          style: TextStyle(color: Colors.white, fontSize: 20),
+    return Expanded(
+      child: OutlinedButton(
+        style: OutlinedButton.styleFrom(
+          shape: CircleBorder(),
+          backgroundColor: bgColor,
+          side: BorderSide.none,
+        ),
+        onPressed: funcion,
+        child: Container(
+          alignment: Alignment.center,
+          height: MediaQuery.of(context).size.height / 10,
+          width: MediaQuery.of(context).size.width / 6,
+          child: Text(
+            texto,
+            style: TextStyle(color: Colors.white, fontSize: 30),
+          ),
         ),
       ),
     );
